8a14fb4243e75f38995eaedda3293fe0f2b623ef
Somya Jaiswal <som.jaiswal.sj@gmail.com>
2026-02-09 13:05:53 +0530

    react config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // Refer to this main Documentation - https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/sharon_always_be_mine/'
 })
